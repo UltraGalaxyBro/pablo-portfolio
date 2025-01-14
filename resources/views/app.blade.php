d47423a3c8c22d9cfabe36a162b97cf589d0a293
Pablo Nogueira <pablonogueira1996@gmail.com>
2025-01-14 13:54:59 -0300

    Fixing v1.00

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Portfólio') }}</title>
-
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.png')}} ">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
